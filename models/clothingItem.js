d22002390225d86b0bf027eb060f062d084114bd
Husam Bahra <husamsrock@gmail.com>
2025-04-25 03:59:14 -0400

    new updates

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,7 +1,37 @@
 const mongoose = require("mongoose");
 
 const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
 
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"], // only these three allowed
+  },
+
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user", // reference to the user model
+    required: true,
+  },
+
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user", //reference to user model
+      default: [],
+    },
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("item", clothingItemSchema);
