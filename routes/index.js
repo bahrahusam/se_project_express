273c91bfc6a497b108ffe64ee0573b84ad316b03
Husam Bahra <115939378+bahrahusam@users.noreply.github.com>
2025-05-04 20:10:10 +0000

    new stuff

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = require("express").Router();
 // const { default: mongoose } = require("mongoose");
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
+const { loginUser, createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
+app.post('/signin', loginUser);
+app.post('/signup', createUser);
 
 module.exports = router;
